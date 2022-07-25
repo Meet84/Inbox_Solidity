0e5c4af7b7d9254ab8081e76ae0544f2f1a482b1
Meet84 <meetu.sokhal@gmail.com>
2022-07-25 13:47:48 +0530

    Updated solc@0.8.9

---
+++ contracts/Inbox.sol
@@ -0,0 +1,17 @@
+
+//SPDX-License-Unlicensed:MIT
+contract Inbox {
+    string public message;
+    
+    constructor(string memory initialMessage) {
+        message = initialMessage;
+    }
+    
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
+
+
+
+
